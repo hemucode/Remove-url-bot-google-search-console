a130644b4dcf98323cc492f81130495b35d817da
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2022-10-29 20:57:15 +0530

    ghf

---
+++ Bloger_Post_Delete.js
@@ -0,0 +1,22 @@
+setInterval(()=>{
+	del_btn = document.querySelectorAll(".Ce1Y1c>.DPvwYc[aria-hidden='true']")[163];
+	Delete_post_1 = document.querySelectorAll(".RveJvd.snByac")[5];
+	Delete_post_1 = document.querySelectorAll(".RveJvd.snByac")[7];
+	if (!document.querySelectorAll(".mjANdc.eEPege")[0]) {
+		if (del_btn) {
+			del_btn.click();
+		}
+	}
+	if (Delete_post_1) {
+		if (Delete_post_1.innerText=='DELETE POST') {
+			Delete_post_1.click();
+		}	
+	}
+	if (Delete_post_2) {
+		if (Delete_post_2.innerText=='DELETE POST') {
+			Delete_post_2.click();
+		}	
+	}
+
+
+},300)
